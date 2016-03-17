a26ff743eef6813ea4c281608a95614a6dbab970
Ephrem Shiferaw <ephrem.shiferaw@outlook.com>
2016-03-17 16:14:57 +0000

    Updated git_sync.ps1

--- git_sync.ps1
+++ git_sync.ps1
@@ -1,6 +1,5 @@
-git remote add githuborigin https://ephremshiferaw:epsh3069@github.com/ephremshiferaw/FMS.git
-git remote set-url --push githuborigin https://ephremshiferaw:epsh3069@github.com/ephremshiferaw/FMS.git
+
+git remote set-url --push origin https://ephremshiferaw:epsh3069@github.com/ephremshiferaw/FMS.git
 # Set the push location to your mirror
 
 git push --mirror
-git remote remove githubOrigin
