a4a3ff04f3edf20636dc6881d2c30d42691b55b3
Ephrem Shiferaw <ephrem.shiferaw@outlook.com>
2016-03-17 20:40:05 +0000

    Updated git_sync.ps1

--- git_sync.ps1
+++ git_sync.ps1
@@ -2,15 +2,14 @@
 Param (
   [Parameter(Mandatory=$True,Position=0)]
   [string]$remoteUrl,
-  
-  [Parameter(Mandatory=$True,Position=1)]
-  [string]$pushTag
 )
 #$remoteUrl is the report git repo path e.g https://username:password@github.com/repo/Project.git
 trap 
 { 
  exit 0
 } 
+} 
+$pushTag=git rev-parse --short HEAD
 git tag $pushTag
 git remote set-url --push origin $remoteUrl
 git push --mirror --force 
