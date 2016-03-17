7db8bbd61733dd9c3ecbe8660fc481f52004dd83
Ephrem Shiferaw <ephrem.shiferaw@outlook.com>
2016-03-17 18:40:28 +0000

    Updated git_sync.ps1

--- git_sync.ps1
+++ git_sync.ps1
@@ -1,12 +1,9 @@
 trap 
 { 
-  write-output $_ 
   exit 1 
 } 
  #cd repository-to-mirror.git
     #git remote add githuborigin https://ephremshiferaw:epsh3069@github.com/ephremshiferaw/FMS.git
-    git remote set-url --push origin https://ephremshiferaw:epsh3069@github.com/ephremshiferaw/FMS.git
-    git push --mirror --force 
-   write $LastExitCode
-   
-   exit 1
+git remote set-url --push origin https://ephremshiferaw:epsh3069@github.com/ephremshiferaw/FMS.git
+git push --mirror --force 
+write $LastExitCode
